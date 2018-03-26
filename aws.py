ad60746c0cf5c26605df2aafa57124b096e15da2
Sergii Pavlenko <itsm@kto.ua>
2018-03-26 19:12:10 +0400

    initial commit

---
+++ aws.py
@@ -0,0 +1,111 @@
+import boto3
+import socket
+import datetime
+
+FQDNs = ['a.itsm.org.ua', 'b.itsm.org.ua', 'c.itsm.org.ua']
+
+PUBLIC_IPS = [socket.gethostbyname(e) for e in FQDNs]
+MAX_AGE = 7
+ec2 = boto3.resource('ec2')
+
+stopped_instances = []
+
+def _spacer(value, columnLength):
+    return (value + ' ' * columnLength)[:columnLength]
+
+def _get_headers():
+    return  [
+        'Name',
+        'Instance ID',
+        'Instance Type',
+        'IPv4 Public IP',
+        'IPv4 Private IP',
+        'Key Name',
+        'Instance State',
+        'Monitoring'
+    ]
+
+def _get_instance_values(instance):
+    return [
+        instance.tags[0]['Value'] if instance.tags else '',
+        instance.id,
+        instance.instance_type,
+        instance.public_ip_address if instance.public_ip_address else '',
+        instance.private_ip_address if instance.private_ip_address else '',
+        instance.key_name,
+        instance.state['Name'],
+        instance.monitoring['State']
+    ]
+
+def _print_headers(headers, columnLength):
+    line = ' | '.join([_spacer(h, columnLength) for h in headers])
+    print line
+
+    line = ' | '.join(['-' * columnLength for h in headers])
+    print line
+
+def _print_row(values, columnLength):
+    line = ' | '.join([_spacer(v, columnLength) for v in values])
+    print line
+
+headers = _get_headers()
+
+print 'Determining instances states ...'
+print('\n')
+_print_headers(['FQDN'] + headers, 18)
+
+for instance in ec2.instances.filter(
+        Filters=[{
+            'Name': 'network-interface.addresses.association.public-ip',
+            'Values': PUBLIC_IPS
+        }]):
+    state = instance.state['Name']
+    fqdn = FQDNs[PUBLIC_IPS.index(instance.public_ip_address)]
+    instance.fqdn = fqdn
+    _print_row([instance.fqdn] + _get_instance_values(instance), 18)
+
+    if state == 'stopped':
+        stopped_instances.append(instance)
+
+print 'Creating AMI for stopped instances ...'
+for si in stopped_instances:
+    name = si.tags[0]['Value'] + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    description = si.tags[0]['Value'] + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    image = ec2.meta.client.create_image( Description=description, InstanceId=si.id, Name=name)
+    tags = ec2.meta.client.create_tags(
+        Resources = [image['ImageId']],
+        Tags = [
+            {
+                'Key': 'Name',
+                'Value': description,
+            },
+        ],
+    )
+
+    # print 'Terminating stopped EC2 instance:', si.id, '...'
+    # response = ec2.meta.client.terminate_instances(
+    #     InstanceIds=[
+    #         si.id
+    #     ]
+    # )
+
+    print 'Clean up AMIs older than 7 days ...'
+    response = ec2.meta.client.describe_images(
+        Owners=[
+            'self',
+        ]
+    )
+    for image in response['Images']:
+        created_on = datetime.datetime.strptime(image['CreationDate'][:-5], '%Y-%m-%dT%H:%M:%S')
+        age = (datetime.datetime.today() - created_on).days
+        if age > MAX_AGE:
+            response = ec2.meta.client.deregister_image(
+                ImageId=image['ImageId']
+            )
+
+print('\n')
+print 'Print all EC2 instances ...'
+print('\n')
+_print_headers(headers, 18)
+for instance in ec2.instances.all():
+    _print_row(_get_instance_values(instance), 18)
